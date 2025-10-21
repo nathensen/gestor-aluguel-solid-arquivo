67203c110a79e5a5f6f0c22880eb59fcdc3ec7b4
Nathensen <nanehensen@gmail.com>
2025-10-21 20:32:48 -0300

    new
    new

--- src/main/java/com/tecdes/aluguel/controller/AluguelController.java
+++ src/main/java/com/tecdes/aluguel/controller/AluguelController.java
@@ -1,5 +1,22 @@
 package com.tecdes.aluguel.controller;
 
+import com.tecdes.aluguel.model.Aluguel;
+
 public class AluguelController {
-    
+
+    public String listarAlugueis() {
+        throw new UnsupportedOperationException("Unimplemented method 'listarAlugueis'");
+    }
+
+    public String processarAluguel(Aluguel aluguel) {
+        throw new UnsupportedOperationException("Unimplemented method 'processarAluguel'");
+    }
+    /////////////////////////////
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
 }
